ebc43648f5e1effc44568f10159f88ec651bac6c
M.Uzair <uzair.iu17@gmail.com>
2020-09-22 16:13:35 +0500

    done with dish detail component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,17 +1,30 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Menu from './Menu';
-import { DISHES  } from '../shared/dishes';
+import DishDetail from './DishDetail'
+import { DISHES } from '../shared/dishes';
+import { View } from 'react-native';
 
-class Main extends Component{
-    constructor(props){
+class Main extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            dishes:DISHES
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
         }
     }
-    render(){
-        return(
-                <Menu dishes={this.state.dishes}/>
+
+    onDishSelect(dishId) {
+        this, this.setState({ selectedDish: dishId })
+    }
+
+    render() {
+        return (
+            <View style={{flex:1}}>
+
+                <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            </View>
+
         )
     }
 }
