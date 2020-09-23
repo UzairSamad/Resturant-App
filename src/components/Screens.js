695c4398e85d5c3c0d678d02074a3ef6cd343fdf
M.Uzair <uzair.iu17@gmail.com>
2020-09-23 13:42:24 +0500

    added proper navigation

--- src/components/Screens.js
+++ src/components/Screens.js
@@ -1,7 +1,8 @@
 import Menu from './Menu'
 import MainComponent from './MainComponent'
 import DishDetail from './DishDetail'
+import Home from './Home'
 
-export { Menu, MainComponent, DishDetail }
+export { Menu, MainComponent, DishDetail ,Home}
 
 
--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -2,19 +2,29 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Menu, DishDetail, MainComponent } from '../components/Screens'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Menu, DishDetail, MainComponent,Home } from '../components/Screens'
 
 
 
 export default function MainNavigator() {
     const Stack = createStackNavigator();
+    const Drawer = createDrawerNavigator()
+
+    const StackScreens = () => {
+        return (
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="List" component={MainComponent} options={{ title: 'Menu',headerTitleAlign: 'center' }} />
+                <Stack.Screen name="Detail" component={DishDetail} options={{ title: 'DishDetail', headerTitleAlign: 'center' }} />
+            </Stack.Navigator>
+        )
+    }
 
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={MainComponent} options={{ headerTitleAlign: 'center' }} />
-                <Stack.Screen name="Detail" component={DishDetail} options={{ title: 'DishDetail', headerTitleAlign: 'center' }} />
-
-            </Stack.Navigator>
+            <Drawer.Navigator>
+                <Drawer.Screen name="Home" component={Home}  options={{ title: 'Home', headerTitleAlign: 'center' }} />
+                <Drawer.Screen name="List"  children={StackScreens}  />
+            </Drawer.Navigator>
         </NavigationContainer>);
 }
