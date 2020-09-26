102f4ab30408cc025c06073fe6a07e1d2a494e11
M.Uzair <uzair.iu17@gmail.com>
2020-09-26 14:45:15 +0500

    done with favourite dish functionality

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { View, Text, ScrollView, FlatList } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
+import { COMMENTS } from '../shared/comments'
+
 
 function RenderDish(props) {
     const dish = props.dish
@@ -13,6 +15,14 @@ function RenderDish(props) {
                 <Text style={{ margin: 10 }}>
                     {dish.description}
                 </Text>
+                <Icon
+                    raised
+                    reverse
+                    name={props.favorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
+                />
             </Card>
 
         )
@@ -27,13 +37,51 @@ function RenderDish(props) {
 
 }
 
+function RenderComments(props) {
+
+    const comments = props.comments;
+
+    const renderCommentItem = ({ item, index }) => {
+
+        return (
+            <View key={index} style={{ margin: 10 }}>
+                <Text style={{ fontSize: 14 }}>{item.comment}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>{'-- ' + item.author + ', ' + item.date} </Text>
+            </View>
+        );
+    };
+
+    return (
+        <Card title='Comments' >
+            <FlatList
+                data={comments}
+                renderItem={renderCommentItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        </Card>
+    );
+}
 
 
 function DishDetail(props) {
+    const [favourites, setFavourite] = React.useState([])
+
     const { item } = props.route.params
     console.log(item, 'dishaa')
+
+    const markFavorite = (dishId) => {
+        setFavourite([dishId])
+    }
+
+
     return (
-        <RenderDish dish={item} />
+        <ScrollView>
+            <RenderDish dish={item} favorite={favourites.some(el => el === item.id)}
+                onPress={() => { markFavorite(item.id) }} />
+            <RenderComments comments={COMMENTS.filter((comment) => comment.dishId === item.id)} />
+        </ScrollView>
+
     )
 }
 
