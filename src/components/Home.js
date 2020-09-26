5c6bc5dc7a24dbe79f0a11c101c62669daa81eac
M.Uzair <uzair.iu17@gmail.com>
2020-09-26 13:28:41 +0500

    added icons in drawer

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,25 +4,27 @@ import { Card } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 import { PROMOTIONS } from '../shared/promotion';
 import { LEADERS } from '../shared/leaders';
+import { Icon } from 'react-native-elements'
+
 
 function RenderItem(props) {
-            const item = props.item;
-        
-        if (item != null) {
-            return(
-                <Card
-                    featuredTitle={item.name}
-                    featuredSubtitle={item.designation}
-                    image={require('../images/uthappizza.png')}>
-                    <Text
-                        style={{margin: 10}}>
-                        {item.description}</Text>
-                </Card>
-            );
-        }
-        else {
-            return(<View></View>);
-        }
+    const item = props.item;
+
+    if (item != null) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                featuredSubtitle={item.designation}
+                image={require('../images/uthappizza.png')}>
+                <Text
+                    style={{ margin: 10 }}>
+                    {item.description}</Text>
+            </Card>
+        );
+    }
+    else {
+        return (<View></View>);
+    }
 }
 
 class Home extends Component {
@@ -30,23 +32,33 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          dishes: DISHES,
-          promotions: PROMOTIONS,
-          leaders: LEADERS
+            dishes: DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
-   
+
 
     render() {
-        
-        return(
-            <ScrollView>
-                <RenderItem item={this.state.dishes.filter((dish) => dish.featured)[0]} />
-                <RenderItem item={this.state.promotions.filter((promo) => promo.featured)[0]} />
-                <RenderItem item={this.state.leaders.filter((leader) => leader.featured)[0]} />
-            </ScrollView>
+
+        return (
+            <View>
+                <Icon
+                    name='menu'
+                    size={24}
+                    onPress={() => this.props.navigation.openDrawer()}
+                // color="red"
+                />
+                <ScrollView>
+                    <RenderItem item={this.state.dishes.filter((dish) => dish.featured)[0]} />
+                    <RenderItem item={this.state.promotions.filter((promo) => promo.featured)[0]} />
+                    <RenderItem item={this.state.leaders.filter((leader) => leader.featured)[0]} />
+                </ScrollView>
+            </View>
         );
+
+
     }
 }
 
--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -3,7 +3,10 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Menu, DishDetail, MainComponent,Home ,About,Contact} from '../components/Screens'
+import { Menu, DishDetail, MainComponent, Home, About, Contact } from '../components/Screens'
+import { Button } from 'react-native'
+import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 
 
@@ -11,22 +14,62 @@ export default function MainNavigator() {
     const Stack = createStackNavigator();
     const Drawer = createDrawerNavigator()
 
-    const StackScreens = () => {
+    const StackScreens = (props) => {
         return (
             <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="List" component={MainComponent} options={{ title: 'Menu',headerTitleAlign: 'center' }} />
+                <Stack.Screen name="Home" component={Home} options={{
+                    title: 'Home', headerTitleAlign: 'center',
+                }} />
+                <Stack.Screen name="List" component={MainComponent} options={{ title: 'List', headerTitleAlign: 'center' }} />
                 <Stack.Screen name="Detail" component={DishDetail} options={{ title: 'DishDetail', headerTitleAlign: 'center' }} />
             </Stack.Navigator>
         )
     }
 
     return (
+
         <NavigationContainer>
             <Drawer.Navigator>
-                <Drawer.Screen name="Home" component={Home}  options={{ title: 'Home', headerTitleAlign: 'center' }} />
-                <Drawer.Screen name="List"  children={StackScreens}  />
-                <Drawer.Screen name="Contact"  component={Contact}  options={{ title: 'About', headerTitleAlign: 'center' }} />
-                <Drawer.Screen name="About"  component={About}  options={{ title: 'About', headerTitleAlign: 'center' }} />
+                <Drawer.Screen name="Home" children={StackScreens} options={{
+                    drawerIcon: ({ focused, size }) => (
+                        <Icon
+                            name="home"
+                            type='font-awesome'
+                            size={size}
+                            color={focused ? '#7cc' : '#ccc'}
+                        />
+                    ),
+                }} />
+                <Drawer.Screen name="Menu" component={MainComponent} options={{
+                    drawerIcon: ({ focused, size }) => (
+                        <Icon
+                            name="list"
+                            type='font-awesome'
+                            size={size}
+                            color={focused ? '#7cc' : '#ccc'}
+                        />
+                    ),
+                }} />
+                <Drawer.Screen name="Contact" component={Contact} options={{
+                    drawerIcon: ({ focused, size }) => (
+                        <Icon
+                            name="address-card"
+                            type='font-awesome'
+                            size={size}
+                            color={focused ? '#7cc' : '#ccc'}
+                        />
+                    ),
+                }} />
+                <Drawer.Screen name="About" component={About} options={{ title: 'About', headerTitleAlign: 'center' }} options={{
+                    drawerIcon: ({ focused, size }) => (
+                        <Icon
+                            name="info-circle"
+                            type='font-awesome'
+                            size={size}
+                            color={focused ? '#7cc' : '#ccc'}
+                        />
+                    ),
+                }} />
             </Drawer.Navigator>
         </NavigationContainer>);
 }
